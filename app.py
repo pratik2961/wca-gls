75f561ab72019b1bc9f725dd20f3ddc7b791d891
pratik2961 <95845222+pratik2961@users.noreply.github.com>
2022-11-11 19:06:47 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -10,224 +10,238 @@ matplotlib.use('Agg')
 st.sidebar.title("Whatsapp Chat Analyzer")
 
 uploaded_file = st.sidebar.file_uploader("Choose a file")
+
 if uploaded_file is not None:
-    bytes_data = uploaded_file.getvalue()
-    data = bytes_data.decode('utf-8')
-    # st.text(data)
-    df = preprocessor.preprocess(data)
-    # st.dataframe(df)
-
-    user_list = df['users'].unique().tolist()
-    if 'group notification' in user_list:
-        user_list.remove('group notification')
-    user_list.sort()
-    user_list.insert(0,"Overall")
-    selected_user = st.sidebar.selectbox('Show analysis wrt',user_list )
-
-    if st.sidebar.button("Show Analysis"):
-        st.title("Top Statistics")
-        num_message,num_words,num_media_msg,link_count = helper.fetch_stats(selected_user,df)
-        col1 , col2 , col3 , col4 = st.columns(4)
-        
-        with col1:
+    if ".txt" in uploaded_file.name:
+        bytes_data = uploaded_file.getvalue()
+        data = bytes_data.decode('utf-8')
+        # st.text(data)
+        df,format_check = preprocessor.preprocess(data)
+        if format_check == False:
+            st.error(" -> You have uploaded file with wrong Time format.")
+            st.error(" -> WCA require only 24 Hour formated text file")
+            st.error(" -> Note 'AM/PM format not working here.....'")
+            st.error(" -> 😥 ")
             
-            st.header("Total Messages")
-            st.title(num_message)
+        else:
+            # st.dataframe(df)
 
-        with col2:
-            st.header("Total Words")
-            st.title(num_words)
+            user_list = df['users'].unique().tolist()
+            if 'group notification' in user_list:
+                user_list.remove('group notification')
+            user_list.sort()
+            user_list.insert(0,"Overall")
+            selected_user = st.sidebar.selectbox('Show analysis wrt',user_list )
 
-        with col3:
-            st.header("Media Shared")
-            st.title(num_media_msg)
+            if st.sidebar.button("Show Analysis"):
+                st.title("Top Statistics")
+                num_message,num_words,num_media_msg,link_count = helper.fetch_stats(selected_user,df)
+                col1 , col2 , col3 , col4 = st.columns(4)
+                
+                with col1:
+                    
+                    st.header("Total Messages")
+                    st.title(num_message)
 
-        with col4:
-            st.header("Total Links")
-            st.title(link_count)
+                with col2:
+                    st.header("Total Words")
+                    st.title(num_words)
 
-        
-        #finding most busiest user
-        if selected_user == "Overall":
-            st.title("Most Busy User")
-            x,df1 = helper.fetch_most_busy_user(df)
-            fig,ax = plt.subplots()
-            name = x.index
-            value = x.values
+                with col3:
+                    st.header("Media Shared")
+                    st.title(num_media_msg)
 
+                with col4:
+                    st.header("Total Links")
+                    st.title(link_count)
+
+                
+                #finding most busiest user
+                if selected_user == "Overall":
+                    st.title("Most Busy User")
+                    x,df1 = helper.fetch_most_busy_user(df)
+                    fig,ax = plt.subplots()
+                    name = x.index
+                    value = x.values
+
+                    
+                    col1 , col2 =  st.columns(2)
+
+                    with col1:
+                        ax.bar(name,value,color='black')
+                        plt.xticks(rotation='vertical')
+                        st.pyplot(fig)
+                    with col2:
+                        new_df = df1.head()
+                        st.dataframe(new_df)
+
+
+                st.title("Most Comman Word")
+                #word cloud
             
-            col1 , col2 =  st.columns(2)
+                st.header("Word Cloud")
+                df_wc = helper.create_word_cloud(selected_user,df)
+                fig,ax = plt.subplots()
+                plt.imshow(df_wc)
+                st.pyplot(fig)
 
-            with col1:
-                ax.bar(name,value,color='black')
+
+                #most common word
+                st.header("Word and Count - bar chart")
+                col1 , col2 = st.columns(2)
+
+                
+                
+                comman_df = helper.top_comman_word(selected_user,df)
+                with col1:
+                    st.dataframe(comman_df)
+                with col2:    
+                    fig,ax = plt.subplots()
+                    ax.bar(comman_df['words'],comman_df['count'],color='black')
+                    plt.xticks(rotation='vertical')
+                    st.pyplot(fig)
+            
+                
+
+
+
+
+                #month time 
+                st.title("Timeline")
+                timeline_df = helper.month_timeline(selected_user,df)
+                st.header("Monthly TimeLine")
+
+                fig,ax = plt.subplots()
+                ax.plot(timeline_df['time'],timeline_df['message'],color='black')
                 plt.xticks(rotation='vertical')
                 st.pyplot(fig)
-            with col2:
-                new_df = df1.head()
-                st.dataframe(new_df)
+                
+
+                #daily timeline
+                st.header("Daily Timeline")
+
+                daily_timeline = helper.daily_timeline(selected_user,df)
+                fig,ax = plt.subplots()
+                
+                ax.plot(daily_timeline['daily_timeline'],daily_timeline['message'],color='black')
+                plt.xticks(rotation='vertical')
+                st.pyplot(fig)
+
+                #most busy month
+                
+                st.title("Activity Map")
+                st.header("Most Busy Month")
+                col1,col2 = st.columns(2)
+                month_count_df = helper.active_month(selected_user,df)
+                with col1:
+                    st.dataframe(month_count_df)
+                with col2:
+                    fig,ax = plt.subplots()
+                    ax.bar(month_count_df['index'],month_count_df['month'],color='black')
+                    st.pyplot(fig)
+
+                st.header('Monthly heat map')
+                heat_map = helper.activity_heatmap(selected_user,df)
+
+                fig,ax = plt.subplots()
+                ax = sns.heatmap(heat_map)
+                st.pyplot(fig)
 
 
-        st.title("Most Comman Word")
-        #word cloud
-    
-        st.header("Word Cloud")
-        df_wc = helper.create_word_cloud(selected_user,df)
-        fig,ax = plt.subplots()
-        plt.imshow(df_wc)
-        st.pyplot(fig)
+                #emoji 
+
+                st.title("Emoji Analysis")
+                emoji_df = helper.emoji_helper(selected_user,df).head()
+
+                col1 , col2 = st.columns(2)
+
+                with col1:
+                    st.dataframe(emoji_df)
+                with col2:
+                    fig,ax = plt.subplots()
+                    ax.pie(emoji_df['count'],labels=emoji_df['emoji'],autopct='%0.2f')
+                    st.pyplot(fig)
 
 
-        #most common word
-        st.header("Word and Count - bar chart")
-        col1 , col2 = st.columns(2)
+                #positive and negative word count
+                st.title("Word Count")
+                positive_count = helper.positive_word_count(selected_user,df)
+                negative_count = helper.negative_word_count(selected_user,df)
+                col1 , col2  = st.columns(2)
+                
+                with col1:
+                    
+                    st.header("Positive Word Count")
+                    st.title(positive_count)
 
-        
-        
-        comman_df = helper.top_comman_word(selected_user,df)
-        with col1:
-            st.dataframe(comman_df)
-        with col2:    
-            fig,ax = plt.subplots()
-            ax.bar(comman_df['words'],comman_df['count'],color='black')
-            plt.xticks(rotation='vertical')
-            st.pyplot(fig)
-    
-        
+                with col2:
+                    st.header("Negative Word Count")
+                    st.title(negative_count)
 
 
+                
+                #positive and negative word dataframe
+                
+                positive_df = helper.positive_word_df(selected_user,df)
+                negative_df = helper.negative_word_df(selected_user,df)
+                col1 , col2  = st.columns(2)
+                
+                with col1:
+                    
+                    st.header("Positive Word With Count")
+                    st.dataframe(positive_df)
+
+                with col2:
+                    st.header("Negative Word With Count")
+                    st.dataframe(negative_df)
+
+                p_df,n_df = helper.comparison_positive_negative(user_list,df)
+
+                
+                col1 , col2 = st.columns(2)
+
+                with col1:
+                    st.title("Positive word comparison")
+                    fig,ax = plt.subplots()
+                    ax.pie(p_df['count'],labels=p_df['name'],autopct='%0.2f')
+                    st.pyplot(fig)
+                with col2:
+                    st.title("Negative word comparison")
+                    fig,ax = plt.subplots()
+                    ax.pie(n_df['count'],labels=n_df['name'],autopct='%0.2f')
+                    st.pyplot(fig)
+
+                #negative positive comaprison for selected user
+                if selected_user != "Overall":
+                    st.title("Positive and Negative Comparison for "+selected_user)
+                    df1 = helper.user_positive_negative(selected_user,df)
+                    fig,ax = plt.subplots()
+                    ax.pie(df1['count'],labels=df1['type'],autopct='%0.2f')
+                    st.pyplot(fig)
 
 
-        #month time 
-        st.title("Timeline")
-        timeline_df = helper.month_timeline(selected_user,df)
-        st.header("Monthly TimeLine")
+                #select positive word
+                positive_df = helper.positive_word_df(selected_user,df)
+                positive_word_list = list(positive_df['word'])
 
-        fig,ax = plt.subplots()
-        ax.plot(timeline_df['time'],timeline_df['message'],color='black')
-        plt.xticks(rotation='vertical')
-        st.pyplot(fig)
-        
-
-        #daily timeline
-        st.header("Daily Timeline")
-
-        daily_timeline = helper.daily_timeline(selected_user,df)
-        fig,ax = plt.subplots()
-        
-        ax.plot(daily_timeline['daily_timeline'],daily_timeline['message'],color='black')
-        plt.xticks(rotation='vertical')
-        st.pyplot(fig)
-
-        #most busy month
-        
-        st.title("Activity Map")
-        st.header("Most Busy Month")
-        col1,col2 = st.columns(2)
-        month_count_df = helper.active_month(selected_user,df)
-        with col1:
-            st.dataframe(month_count_df)
-        with col2:
-            fig,ax = plt.subplots()
-            ax.bar(month_count_df['index'],month_count_df['month'],color='black')
-            st.pyplot(fig)
-
-        st.header('Monthly heat map')
-        heat_map = helper.activity_heatmap(selected_user,df)
-
-        fig,ax = plt.subplots()
-        ax = sns.heatmap(heat_map)
-        st.pyplot(fig)
+                st.header("Positive word and messages : ")
+                for i in range(0,len(positive_word_list)):
+                    st.text("Word is : "+positive_word_list[i])
+                    new_df = helper.list_of_message(selected_user,df,positive_word_list[i])
+                    st.dataframe(new_df)
 
 
-        #emoji 
+                #select negative word
+                negative_df = helper.negative_word_df(selected_user,df)
+                negative_word_list = list(negative_df['word'])
 
-        st.title("Emoji Analysis")
-        emoji_df = helper.emoji_helper(selected_user,df).head()
+                st.header("Negative word and messages : ")
+                for i in range(0,len(negative_word_list)):
+                    st.text("Word is : "+negative_word_list[i])
+                    new_df = helper.list_of_message(selected_user,df,negative_word_list[i])
+                    st.dataframe(new_df)
 
-        col1 , col2 = st.columns(2)
-
-        with col1:
-            st.dataframe(emoji_df)
-        with col2:
-            fig,ax = plt.subplots()
-            ax.pie(emoji_df['count'],labels=emoji_df['emoji'],autopct='%0.2f')
-            st.pyplot(fig)
-
-
-        #positive and negative word count
-        st.title("Word Count")
-        positive_count = helper.positive_word_count(selected_user,df)
-        negative_count = helper.negative_word_count(selected_user,df)
-        col1 , col2  = st.columns(2)
-        
-        with col1:
-            
-            st.header("Positive Word Count")
-            st.title(positive_count)
-
-        with col2:
-            st.header("Negative Word Count")
-            st.title(negative_count)
-
-
-        
-        #positive and negative word dataframe
-        
-        positive_df = helper.positive_word_df(selected_user,df)
-        negative_df = helper.negative_word_df(selected_user,df)
-        col1 , col2  = st.columns(2)
-        
-        with col1:
-            
-            st.header("Positive Word With Count")
-            st.dataframe(positive_df)
-
-        with col2:
-            st.header("Negative Word With Count")
-            st.dataframe(negative_df)
-
-        p_df,n_df = helper.comparison_positive_negative(user_list,df)
-
-        
-        col1 , col2 = st.columns(2)
-
-        with col1:
-            st.title("Positive word comparison")
-            fig,ax = plt.subplots()
-            ax.pie(p_df['count'],labels=p_df['name'],autopct='%0.2f')
-            st.pyplot(fig)
-        with col2:
-            st.title("Negative word comparison")
-            fig,ax = plt.subplots()
-            ax.pie(n_df['count'],labels=n_df['name'],autopct='%0.2f')
-            st.pyplot(fig)
-
-        #negative positive comaprison for selected user
-        if selected_user != "Overall":
-            st.title("Positive and Negative Comparison for "+selected_user)
-            df1 = helper.user_positive_negative(selected_user,df)
-            fig,ax = plt.subplots()
-            ax.pie(df1['count'],labels=df1['type'],autopct='%0.2f')
-            st.pyplot(fig)
-
-
-        #select positive word
-        positive_df = helper.positive_word_df(selected_user,df)
-        positive_word_list = list(positive_df['word'])
-
-        st.header("Positive word and messages : ")
-        for i in range(0,len(positive_word_list)):
-            st.text("Word is : "+positive_word_list[i])
-            new_df = helper.list_of_message(selected_user,df,positive_word_list[i])
-            st.dataframe(new_df)
-
-
-        #select negative word
-        negative_df = helper.negative_word_df(selected_user,df)
-        negative_word_list = list(negative_df['word'])
-
-        st.header("Negative word and messages : ")
-        for i in range(0,len(negative_word_list)):
-            st.text("Word is : "+negative_word_list[i])
-            new_df = helper.list_of_message(selected_user,df,negative_word_list[i])
-            st.dataframe(new_df)
+    else:
+        st.error(" -> You have uploaded file with wrong extension.")
+        st.error(" -> WCA require only text file created by whatsapp for Analysis")
+        st.error(" -> 😁 ")
--- preprocessor.py
+++ preprocessor.py
@@ -1,86 +1,91 @@
 import re
 import pandas as pd
 def preprocess(data):
+    temp = data.split("-")
     
-    pattern='\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s'
-    messages = re.split(pattern,data)[1:]
-    dates = re.findall(pattern,data)
-    df = pd.DataFrame({'user_message':messages,'message_date':dates})
-    df.rename(columns = {'message_date':'date'},inplace=True)
-    users = []
-    messages = []
-    for message in df['user_message']:
-        entry =  re.split('([\w\W]+?):\s',message)
-        if entry[1:]:
-            users.append(entry[1])
-            messages.append(entry[2])
-        else:
-            users.append('group notification')
-            messages.append(entry[0])
+    if("PM" in temp[0] or "AM" in temp[0]):
+        return "",False
+    else:
+    
+        pattern='\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s'
+        messages = re.split(pattern,data)[1:]
+        dates = re.findall(pattern,data)
+        df = pd.DataFrame({'user_message':messages,'message_date':dates})
+        df.rename(columns = {'message_date':'date'},inplace=True)
+        users = []
+        messages = []
+        for message in df['user_message']:
+            entry =  re.split('([\w\W]+?):\s',message)
+            if entry[1:]:
+                users.append(entry[1])
+                messages.append(entry[2])
+            else:
+                users.append('group notification')
+                messages.append(entry[0])
+                
+        df['users'] = users
+        df['message'] = messages
+        df.drop(columns=['user_message'],inplace=True)
             
-    df['users'] = users
-    df['message'] = messages
-    df.drop(columns=['user_message'],inplace=True)
-        
-    year = []
-    month = []
-    day = []
-    hour = []
-    minute = []
-    month_num = []
-    for date in df['date']:
-        date1 = date.split(",")
-        date2 = date1[0].split("/")
-        year.append(date2[2])
-        month_num.append(date2[0])
-        if date2[0] == '1':
-            month.append("Jan")
-        elif date2[0] == '2':
-            month.append("Feb")
-        elif date2[0] == '3':
-            month.append("March")
-        elif date2[0] == '4':
-            month.append("Apr")
-        elif date2[0] == '5':
-            month.append("May")
-        elif date2[0] == '6':
-            month.append("June")
-        elif date2[0] == '7':
-            month.append("July")
-        elif date2[0] == '8':
-            month.append("August")
-        elif date2[0] == '9':
-            month.append("Sep")
-        elif date2[0] == '10':
-            month.append("Oct")
-        elif date2[0] == '11':
-            month.append("Nov")
-        elif date2[0] == '12':
-            month.append("Dec")
+        year = []
+        month = []
+        day = []
+        hour = []
+        minute = []
+        month_num = []
+        for date in df['date']:
+            date1 = date.split(",")
+            date2 = date1[0].split("/")
+            year.append(date2[2])
+            month_num.append(date2[0])
+            if date2[0] == '1':
+                month.append("Jan")
+            elif date2[0] == '2':
+                month.append("Feb")
+            elif date2[0] == '3':
+                month.append("March")
+            elif date2[0] == '4':
+                month.append("Apr")
+            elif date2[0] == '5':
+                month.append("May")
+            elif date2[0] == '6':
+                month.append("June")
+            elif date2[0] == '7':
+                month.append("July")
+            elif date2[0] == '8':
+                month.append("August")
+            elif date2[0] == '9':
+                month.append("Sep")
+            elif date2[0] == '10':
+                month.append("Oct")
+            elif date2[0] == '11':
+                month.append("Nov")
+            elif date2[0] == '12':
+                month.append("Dec")
 
 
+            
+            day.append(date2[1])
+            date3 = date1[1].split(":")
+            hour.append(date3[0])
+            minute.append(date3[1][0:2])
+            
+        df['year'] = year
+        df['month'] = month
+        df['day'] = day
+        df['hour'] = hour
+        df['minute'] = minute
+        df['month_num'] = month_num
         
-        day.append(date2[1])
-        date3 = date1[1].split(":")
-        hour.append(date3[0])
-        minute.append(date3[1][0:2])
-        
-    df['year'] = year
-    df['month'] = month
-    df['day'] = day
-    df['hour'] = hour
-    df['minute'] = minute
-    df['month_num'] = month_num
-    
-    period = []
-    for hour in df['hour']:
-        if hour == 23:
-            period.append(str(hour)+"-"+str('00'))
-        elif hour == 0:
-            period.append(str('00')+"-"+str(hour+1))
-        else:
-    #         print(type(hour))
-            period.append(str(hour)+"-"+str(int(hour)+1))
-    df['period'] = period
+        period = []
+        for hour in df['hour']:
+            if hour == 23:
+                period.append(str(hour)+"-"+str('00'))
+            elif hour == 0:
+                period.append(str('00')+"-"+str(hour+1))
+            else:
+        #         print(type(hour))
+                period.append(str(hour)+"-"+str(int(hour)+1))
+        df['period'] = period
 
-    return df
+        return df,True
